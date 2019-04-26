c6dfb649153d6d7d9443be98de01f7991382c07c
tthsqe12 <tthsqe12@gmail.com>
2019-04-26 21:30:06 +0200

    fix buggy fallback

--- test/t-add_ssssaaaaaaaa.c
+++ test/t-add_ssssaaaaaaaa.c
@@ -45,10 +45,34 @@ int main(void)
         if (!result)
         {
             flint_printf("FAIL:\n");
-            flint_printf("a[3] = %wu, a[2] = %wu, a[1] = %wu, a[0] = %wu\n", a[2], a[2], a[1], a[0]); 
-            flint_printf("b[3] = %wu, b[2] = %wu, b[1] = %wu, b[0] = %wu\n", b[3], b[2], b[1], b[0]); 
-            flint_printf("s[3] = %wu, s[2] = %wu, s[1] = %wu, s[0] = %wu\n", s[3], s[2], s[1], s[0]); 
-            flint_printf("t[3] = %wu, t[2] = %wu, t[1] = %wu, t[0] = %wu\n", t[3], t[2], t[1], t[0]); 
+            flint_printf("a[3] = %wu, a[2] = %wu, a[1] = %wu, a[0] = %wu\n", a[3], a[2], a[1], a[0]);
+            flint_printf("b[3] = %wu, b[2] = %wu, b[1] = %wu, b[0] = %wu\n", b[3], b[2], b[1], b[0]);
+            flint_printf("s[3] = %wu, s[2] = %wu, s[1] = %wu, s[0] = %wu\n", s[3], s[2], s[1], s[0]);
+            flint_printf("t[3] = %wu, t[2] = %wu, t[1] = %wu, t[0] = %wu\n", t[3], t[2], t[1], t[0]);
+            flint_abort();
+        }
+
+        for (j = 0; j < 4; j++)
+        {
+            s[j] = n_randtest(state);
+            t[j] = n_randtest(state);
+            a[j] = n_randtest(state);
+            b[j] = n_randtest(state);
+        }
+
+        mpn_add_n(t, s, b, 4);
+
+        add_ssssaaaaaaaa(s[3], s[2], s[1], s[0], s[3], s[2], s[1], s[0],
+                                                 b[3], b[2], b[1], b[0]);
+
+        result = ((s[3] == t[3]) && (s[2] == t[2]) && (s[1] == t[1]) && (s[0] == t[0]));
+        if (!result)
+        {
+            flint_printf("FAIL:\naliasing\n");
+            flint_printf("a[3] = %wu, a[2] = %wu, a[1] = %wu, a[0] = %wu\n", a[3], a[2], a[1], a[0]);
+            flint_printf("b[3] = %wu, b[2] = %wu, b[1] = %wu, b[0] = %wu\n", b[3], b[2], b[1], b[0]);
+            flint_printf("s[3] = %wu, s[2] = %wu, s[1] = %wu, s[0] = %wu\n", s[3], s[2], s[1], s[0]);
+            flint_printf("t[3] = %wu, t[2] = %wu, t[1] = %wu, t[0] = %wu\n", t[3], t[2], t[1], t[0]);
             flint_abort();
         }
     }
